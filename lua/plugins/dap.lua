9839e89da16900e06c666f9972e094e142c95052
Ricardo González Moreno <ricardo@richiware.dev>
2024-06-29 11:29:51 +0200

    dap: use of gdb
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,35 @@
+return {
+	"mfussenegger/nvim-dap",
+	config = function()
+		local dap = require("dap")
+		dap.adapters.gdb = {
+			type = "executable",
+			command = "gdb",
+			args = { "-i", "dap" },
+		}
+		dap.configurations.c = {
+			{
+				name = "Launch",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtBeginningOfMainSubprogram = false,
+			},
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtBeginningOfMainSubprogram = false,
+			},
+		}
+	end,
+}
