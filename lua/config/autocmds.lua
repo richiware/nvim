996af5df7ba2ad854cf338d9b74fd86188bafe45
Ricardo González Moreno <ricardo@richiware.dev>
2024-08-19 10:28:29 +0200

    Avoid temporary files when using gopass
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Avoid temporary files when using gopass
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = { "/dev/shm/gopass*" },
+	callback = function(ev)
+		local lo = vim.opt_local
+		lo.swapfile = false
+		lo.backup = false
+		lo.undofile = false
+		lo.shada = ""
+	end,
+})
