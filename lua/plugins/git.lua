11ef8a260e86a28ae49cb3468dfbc04b1a0ceb9f
Ricardo González Moreno <ricardo@richiware.dev>
2024-12-26 08:26:36 +0100

    Add plugins to manage Git repos
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

---
+++ lua/plugins/git.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"sindrets/diffview.nvim",
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		config = true,
+	},
+}
