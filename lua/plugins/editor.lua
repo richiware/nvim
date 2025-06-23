179db1982bf99a85fb7dc21268e472a643ab31de
Ricardo González Moreno <ricardo@richiware.dev>
2025-06-23 08:20:31 +0200

    snacks: set zen-mode's window to 130
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,13 @@
+return {
+	-- Increase the width of the Zen mode
+	{
+		"snacks.nvim",
+		opts = {
+			zen = {
+				win = {
+					width = 130, -- Ancho del área enfocada (por defecto es 80)
+				},
+			},
+		},
+	},
+}
